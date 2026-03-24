03da8d39a21756c0e37b43e3672b437110c6a8d6
Matteo Simone Borghi <Bogghi@users.noreply.github.com>
2026-03-24 23:24:59 +0100

    added animation to pages

--- apps/laundry_app/lib/main.dart
+++ apps/laundry_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:laundry_app/providers/app_provider.dart';
+
+import 'package:laundry_app/utils/navigation_controller.dart';
 
 void main() {
   runApp(
@@ -16,34 +17,9 @@ class App extends ConsumerWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final appState = ref.watch(appProvider);
-    final appStoreProvider = ref.read(appProvider.notifier);
-
     return MaterialApp(
       title: 'Laundry App',
-      home: Scaffold(
-          bottomNavigationBar: NavigationBar(
-            onDestinationSelected: (int index) {
-              appStoreProvider.changePage(index);
-            },
-            selectedIndex: appState.currentIndex,
-            destinations: const [
-              NavigationDestination(
-                selectedIcon: Icon(Icons.list_outlined),
-                icon: Icon(Icons.list_outlined),
-                label: 'Ordini',
-              ),
-              NavigationDestination(
-                selectedIcon: Icon(Icons.settings),
-                icon: Icon(Icons.settings),
-                label: 'Impostazioni',
-              ),
-            ],
-          ),
-          body: Center(
-            child: Text("Home"),
-          ),
-      ),
+      home: const NavigationController(),
     );
   }
 }
---
+++ apps/laundry_app/lib/utils/navigation_controller.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:laundry_app/providers/app_provider.dart';
+import 'package:laundry_app/presentations/screens/home/home_page.dart';
+import 'package:laundry_app/presentations/screens/settings/settings_page.dart';
+import 'package:laundry_app/presentations/screens/home/widgets/add_order_action.dart';
+
+class NavigationController extends ConsumerStatefulWidget {
+  const NavigationController({super.key});
+
+  @override
+  ConsumerState<NavigationController> createState() => _NavigationControllerState();
+}
+
+class _NavigationControllerState extends ConsumerState<NavigationController> {
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final appState = ref.watch(appProvider);
+    final appStoreProvider = ref.read(appProvider.notifier);
+
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          _pageController.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+          );
+          appStoreProvider.changePage(index);
+        },
+        selectedIndex: appState.currentIndex,
+        destinations: const [
+          NavigationDestination(
+            selectedIcon: Icon(Icons.list_outlined),
+            icon: Icon(Icons.list_outlined),
+            label: 'Ordini',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.settings),
+            icon: Icon(Icons.settings),
+            label: 'Impostazioni',
+          ),
+        ],
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          appStoreProvider.changePage(index);
+        },
+        children: const [
+          HomePage(),
+          SettingsPage(),
+        ],
+      ),
+      floatingActionButton: _buildFloatingActionButton(appState.currentIndex),
+    );
+  }
+
+  Widget? _buildFloatingActionButton(int currentIndex) {
+    switch (currentIndex) {
+      case 0:
+        return const AddOrderAction();
+      default:
+        return null;
+    }
+  }
+}
